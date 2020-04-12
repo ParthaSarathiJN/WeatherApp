11152b7139883dd5528ae34f0701cf74cc846c1d
Parth <50483871+ParthaSarathiJN@users.noreply.github.com>
2020-04-13 00:23:37 +0530

    Add files via upload

---
+++ apiInteracter.py
@@ -0,0 +1,45 @@
+import requests
+from decimal import *
+
+# Insert the API key in APIKEY.
+
+
+def WeatherFunction(PinCode, CountryCode):
+
+
+	r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?zip={PinCode},{CountryCode}&appid=APIKEY')
+	weatherValues = r.json()
+
+	# Checks for 4xx and 5xx errors
+	if str(r.status_code)[2] == '4' or str(r.status_code)[2] == '5':
+
+		ErrorCode = weatherValues['cod']
+		result = f'Incorrect Pin Code or Country Abbreviation!\nError Code: {ErrorCode}'
+
+	else:
+
+		weatherKey = weatherValues['main']
+		crntTemperature = weatherKey['temp']
+		#crntPressure = weatherKey['pressure']
+		crntMinTemp = weatherKey['temp_min']
+		crntMaxTemp = weatherKey['temp_max']
+		crntHumidity = weatherKey['humidity']
+
+		#crntVisibility = weatherValues['visibility']
+
+		#weatherWind = weatherValues['wind']
+		#crntWindSpeed = weatherWind['speed']
+
+		#weatherClouds = weatherValues['clouds']
+		#crntClouds = weatherClouds['all']
+
+		weatherWeather = weatherValues['weather']
+		crntMain = weatherWeather[0]['main']
+		crntDescription = weatherWeather[0]['description']
+
+		result = f'Temperature = {"{:.2f}".format(crntTemperature-273.15)}°C,  Weather = {crntMain}({crntDescription}),  Humidity = {crntHumidity}%,\nMin.Temp = {"{:.2f}".format(crntMinTemp-273.15)}°C,  Max.Temp = {"{:.2f}".format(crntMaxTemp-273.15)}°C'
+
+	return result
+
+
+# print(resultOfWeather)
